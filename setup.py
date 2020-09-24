06ddd6f6eba27a8d6944e929d8f477c7bb4cf6dd
rubens <rubensrissi@hotmail.com>
2020-09-24 14:53:16 -0300

    Add on the setup the two possible nedded libraries.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='train_persondet',
+setup(
+      name='yolov3_tf2',
+      version='0.1',
+      url='https://github.com/retail-intelligence/yolov3-tf2',
+      packages=find_packages(exclude='tests')
+)
+
+setup(
+      name='tools',
       version='0.1',
       url='https://github.com/retail-intelligence/yolov3-tf2',
       packages=find_packages(exclude='tests')
