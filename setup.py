361eb19de6c14f3591e9a3420d2df59da9631f1d
rubens <rubensrissi@hotmail.com>
2020-09-25 08:15:32 -0300

    Fix the setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
       name='yolov3_tf2',
       version='0.1',
       url='https://github.com/retail-intelligence/yolov3-tf2',
-      packages=find_packages(exclude='tests')
-)
-
-setup(
-      name='tools',
-      version='0.1',
-      url='https://github.com/retail-intelligence/yolov3-tf2',
-      packages=find_packages(exclude='tests')
+      packages=['yolov3_tf2', 'tools']
 )
